d62c41fe0bdd5293ed6a19c857ed0cdf863d7781
Marcus Bergo <eu@mbergo.dev>
2023-02-13 23:23:55 -0300

    fixing tests

--- src/oh-plugin_test.go
+++ src/oh-plugin_test.go
@@ -2,91 +2,58 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"bytes"
+	"io/ioutil"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"strings"
 	"testing"
 )
 
-func TestInstall(t *testing.T) {
-	repositoryAddress := "https://github.com/example/zsh-plugin"
-	zshCustomDir := filepath.Join(os.Getenv("HOME"), ".oh-my-zsh", "custom", "plugins")
-	zshrcPath := filepath.Join(os.Getenv("HOME"), ".zshrc")
+func TestInstallPlugin(t *testing.T) {
+	// Prepare test inputs and expected outputs
+	input := "https://github.com/some-plugin"
+	expectedOutput := "Plugin installed successfully"
 
-	// Mocked git clone command
-	cloneCalled := false
-	execCommand = func(command string, args ...string) *exec.Cmd {
-		if command == "git" && args[0] == "clone" {
-			cloneCalled = true
-			return exec.Command("echo", "Cloning repository...")
+	// Set up a fake file system for testing
+	oldExecCommand := exec.Command
+	defer func() { exec.Command = oldExecCommand }()
+	execCommand = func(name string, arg ...string) *exec.Cmd {
+		return &exec.Cmd{
+			Stdout: &bytes.Buffer{},
+			Stderr: &bytes.Buffer{},
 		}
-		return exec.Command(command, args...)
 	}
 
-	// Mocked ~/.zshrc file content
-	zshrcContent := ""
-	readFile = func(filename string) ([]byte, error) {
-		return []byte(zshrcContent), nil
+	oldReadFile := ioutil.ReadFile
+	defer func() { ioutil.ReadFile = oldReadFile }()
+	ioutil.ReadFile = func(filename string) ([]byte, error) {
+		return []byte(""), nil
 	}
-	writeFile = func(filename string, data []byte, perm os.FileMode) error {
-		zshrcContent = string(data)
+
+	oldWriteFile := ioutil.WriteFile
+	defer func() { ioutil.WriteFile = oldWriteFile }()
+	ioutil.WriteFile = func(filename string, data []byte, perm os.FileMode) error {
 		return nil
 	}
 
-	// Mocked README.md file content
-	readmeContent := "## Installation\ninstall-command1\ninstall-command2\n"
-	openFile = func(filename string, flag int, perm os.FileMode) (*os.File, error) {
-		file := &os.File{}
-		file.Read = func(p []byte) (int, error) {
-			return strings.NewReader(readmeContent).Read(p)
-		}
-		file.Close = func() error { return nil }
-		return file, nil
+	oldOpenFile := os.OpenFile
+	defer func() { os.OpenFile = oldOpenFile }()
+	os.OpenFile = func(name string, flag int, perm os.FileMode) (*os.File, error) {
+		return &os.File{}, nil
 	}
 
-	os.Args = []string{"oh-plugin", "install", repositoryAddress}
-	main()
-
-	// Check if the git clone command was called
-	if !cloneCalled {
-		t.Error("git clone command was not called")
+	oldNewScanner := bufio.NewScanner
+	defer func() { bufio.NewScanner = oldNewScanner }()
+	bufio.NewScanner = func(r io.Reader) *bufio.Scanner {
+		return &bufio.Scanner{}
 	}
 
-	// Check if the plugin was added to the ~/.zshrc file
-	if !strings.Contains(zshrcContent, "plugins=(zsh-plugin)\n") {
-		t.Error("Plugin was not added to the ~/.zshrc file")
-	}
-}
+	// Call the function being tested
+	output := installPlugin(input)
 
-var execCommand func(command string, args ...string) *exec.Cmd
-
-func TestMain(m *testing.M) {
-	execCommand = exec.Command
-	defer func() { execCommand = exec.Command }()
-	exec.Command = func(command string, args ...string) *exec.Cmd {
-		return execCommand(command, args...)
-	}
-
-	os.Exit(m.Run())
-}
-
-var writeFile func(filename string, data []byte, perm os.FileMode) error
-
-func init() {
-	writeFile = ioutil.WriteFile
-	ioutil.WriteFile = func(filename string, data []byte, perm os.FileMode) error {
-		return writeFile(filename, data, perm)
-	}
-}
-
-var openFile func(filename string, flag int, perm os.FileMode) (*os.File, error)
-
-func init() {
-	openFile = os.OpenFile
-	os.OpenFile = func(filename string, flag int, perm os.FileMode) (*os.File, error) {
-		return openFile(filename, flag, perm)
+	// Compare the actual output to the expected output
+	if output != expectedOutput {
+		t.Errorf("Test failed: expected output %q, got %q", expectedOutput, output)
 	}
 }
 
